d54d6f4beccda4d7a4a81722040c31dbbf428f20
Daniel Williams <danielfrwilliams@gmail.com>
2021-11-23 15:06:39 -0500

    Modify models and create TaskManagerAPI proj

---
+++ CIS4930/Assignment-4/TaskManagerAPI/TaskManagerAPI/Persistance/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagerAPI.Persistance
+{
+    public class Database
+    {
+        public static List<Item> Items = new List<Item>
+    }
+}
